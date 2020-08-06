927ecc04813c6431a55a92d5e8448083805b25a1
Farouq Syed Abdali <farouqsyedabdali@gmail.com>
2020-08-05 22:11:07 -0400

    added more functionality
    added functionality to search and edit contacts and created functions for those. also updated the README file

--- python learning projects/Contact Book/app.py
+++ python learning projects/Contact Book/app.py
@@ -11,9 +11,11 @@ def mainMenu():
     print("1. Read all of your contacts")
     print("2. Add a new contact")
     print("3. Remove a contact")
-    print("4. Quit\n")
+    print("4. Search for a contact")
+    print("5. Edit a contact")
+    print("6. Quit\n")
 
-    while True:
+    while True: #Error checks the input
       try:
         selection = int(input("Select an option: "))
         break
@@ -26,26 +28,53 @@ def mainMenu():
       addContact()
     elif selection == 3:
       removeContact()
-    elif selection == 4: #Quits the program
+    elif selection == 4:
+      searchContact()
+    elif selection == 5:
+      editContact()
+    elif selection == 6: #Quits the program
       print("\nExiting...\n")
       break
+    else:
+      print("\nPlease make a vaild selection!")
 
 def readContacts():
-  print(contact_list)
+  if len(contact_list) == 0:
+    print("\nYou have no contacts")
+  else:
+    print(*contact_list, sep=", ") #Prints contact list without brackets and seperates with comma
 
 def addContact():
-  add_name = input("What is the name of your contact? ")
+  add_name = input("\nWhat is the name of your contact? ")
   contact_list.append(add_name)
-  print(f"{add_name} added to contacts")
+  print(f"\n{add_name} added to contacts")
 
 
 def removeContact():
-  removed_name = input("Who do you want to remove? ")
-  double_check = input("Are you sure you want to remove this person? Y/N: ").lower()
+  removed_name = input("\nWho do you want to remove? ")
+  double_check = input("\nAre you sure you want to remove this person? Y/N: ").lower()
   if double_check == "y": #Double checks with the user
     contact_list.remove(removed_name)
-    print(f"{removed_name} removed from contacts")
+    print(f"\n{removed_name} removed from contacts")
   else:
     pass
 
-mainMenu()
+def searchContact():
+  find_name = input("\nWho do you want to look for? ")
+  if find_name in contact_list:
+    print(f"\n{find_name} is in your contacts")
+  else:
+    print("\nNo results found")
+
+def editContact():
+  while True:
+    old_name = input("\nWho do you want to edit? ")
+    new_name = input("\nWhat's their new name? ")
+    try: #Error checks the input
+      contact_list.remove(old_name)
+      contact_list.append(new_name)
+      break
+    except:
+      print(f"\nThere is no one named {old_name} in your contacts. Try again!")
+
+mainMenu() #Runs main menu function that starts the program
